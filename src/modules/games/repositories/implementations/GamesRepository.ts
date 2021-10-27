fe2dea49886dfcd8f3f9a7af47affb846e206d38
Lucas Coronel <lccoronel7@icloud.com>
2021-10-27 19:28:03 -0300

    feat: user and games

--- src/modules/games/repositories/implementations/GamesRepository.ts
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -14,17 +14,23 @@ export class GamesRepository implements IGamesRepository {
 
   async findByTitleContaining(param: string): Promise<Game[]> {
     return this.repository
-      .createQueryBuilder()
+      .createQueryBuilder("games")
+      .where("games.title ilike :title", { title: `%${param}%` })
+      .getMany();
       // Complete usando query builder
   }
 
   async countAllGames(): Promise<[{ count: string }]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query("SELECT count(*) FROM games"); // Complete usando raw query
   }
 
   async findUsersByGameId(id: string): Promise<User[]> {
     return this.repository
-      .createQueryBuilder()
+      .createQueryBuilder("games")
+      .innerJoinAndSelect("games.users", "users")
+      .where("games.id = :id", { id })
+      .getOne()
+      .then((game) => game?.users ?? []);
       // Complete usando query builder
   }
 }
